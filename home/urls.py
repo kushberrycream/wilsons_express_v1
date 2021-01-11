384e5c4204eaf61f848ca2d97c7579e55670323c
kushberrycream <tom@wilson-express.co.uk>
2021-01-11 22:40:27 +0000

    added covid alert banner and page with apc covid info also added link to apc t&cs

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('covid/', views.covid, name='covid'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index / home page """
     return render(request, 'home/index.html')
+
+
+def covid(request):
+    """ A view to return the covid page """
+    return render(request, 'home/covid.html')
