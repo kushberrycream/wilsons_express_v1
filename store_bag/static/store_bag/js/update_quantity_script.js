8fb1a84260bf4b3035cee02b978abb1367c24db4
kushberrycream <tom@wilson-express.co.uk>
2021-01-13 01:18:47 +0000

    added, update and remove quanity on bag items, added csrf token variable to bag template

--- store_bag/static/store_bag/js/update_quantity_script.js
+++ store_bag/static/store_bag/js/update_quantity_script.js
@@ -6,11 +6,10 @@ $('.update-link').click(function (e) {
 
 // Remove item and reload on click
 $('.remove-item').click(function (e) {
-    let csrfToken = "{{ csrf_token }}";
     let itemId = $(this).attr('id').split('remove_')[1];
     let size = $(this).data('product_size');
     let url = `/bag/remove/${itemId}/`;
-    let data = { 'csrfmiddlewaretoken': csrfToken, 'product_size': size };
+    let data = { 'csrfmiddlewaretoken': window.csrf_token, 'product_size': size };
 
     $.post(url, data)
         .done(function () {
