58d1be697346e04e2a896404cdd50e0b47a685f6
kushberrycream <tom@wilson-express.co.uk>
2021-02-02 02:09:02 +0000

    added product admin views, urls and templates

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.all_categories, name='store_home'),
     path('packing/', views.all_products, name='categories'),
     path('all_products/', views.all_products, name='products'),
-    path('<product_id>/', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
