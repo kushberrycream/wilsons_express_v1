e3d5fae374ba246032af30ae3730291b4a99c9de
kushberrycream <tom@wilson-express.co.uk>
2021-02-08 21:59:25 +0000

    added loading overlay and improved contact form success page

--- contact_form/views.py
+++ contact_form/views.py
@@ -23,4 +23,18 @@ def contact_view(request):
 
 
 def success_view(request):
-    return render(request, 'contact_form/success.html')
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email,
+                          ['admin@wilsons-express.co.uk'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, 'contact_form/success.html', {'form': form})
