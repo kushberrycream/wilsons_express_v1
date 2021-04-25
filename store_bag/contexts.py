04fe4e4113425fa4aedb700476a296b37de128cb
kushberrycream <tom@wilson-express.co.uk>
2021-04-25 10:28:00 +0000

    added bag functionality with quote/booking forms, also added 10% discount to bag

--- store_bag/contexts.py
+++ store_bag/contexts.py
@@ -1,44 +1,45 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from products.models import Product
+from quote.models import Quote
 
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
-    product_count = 0
     bag = request.session.get('bag', {})
+    product_count = 0
 
     for item_id, item_data in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += item_data * product.price
-        subtotal = item_data * product.price
-        product_count += item_data
+        quote = get_object_or_404(Quote, quote_ref=item_id)
+        total += quote.quote
+        subtotal = quote.quote
         bag_items.append({
-            'item_id': item_id,
-            'quantity': item_data,
-            'product': product,
+            'quote_ref': item_id,
+            'weight': quote.overall_weight,
+            'volume_weight': quote.overall_volume,
+            'quote': quote,
             'subtotal': subtotal,
         })
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    else:
-        delivery = 0
-        free_delivery_delta = 0
+    product_count += len(bag_items)
 
-    grand_total = delivery + total
+    if product_count >= settings.TEN_PERCENT_THRESHOLD:
+        ten_percent = total / 10
+        ten_percent_delta = 0
+    else:
+        ten_percent = 0
+        ten_percent_delta = settings.TEN_PERCENT_THRESHOLD - product_count
+
+    grand_total = total - ten_percent
 
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
-        'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'ten_percent_delta': ten_percent_delta,
+        'ten_percent_threshold': settings.TEN_PERCENT_THRESHOLD,
         'grand_total': grand_total,
     }
 
--- store_bag/views.py
+++ store_bag/views.py
@@ -1,7 +1,9 @@
+from decimal import Decimal
 from django.shortcuts import (
     render, redirect, reverse, HttpResponse, get_object_or_404
 )
 from django.contrib import messages
+from quote.models import Quote
 from products.models import Product
 
 
@@ -14,22 +16,17 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shoppingbag """
 
-    product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+    quote = get_object_or_404(Quote, quote_ref=item_id)
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-        messages.success(request, f'Updated {product.name}\
-                             quantity to {bag[item_id]}')
-    else:
-        bag[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your bag')
+    bag[item_id] = item_id
+    messages.success(request,
+                     f'Added collection from \
+    {quote.c_postcode} to your bag')
 
     request.session['bag'] = bag
 
-    return redirect(redirect_url)
+    return redirect(reverse('home'))
 
 
 def adjust_bag(request, item_id):
