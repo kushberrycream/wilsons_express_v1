fd2665c0479503a8343e0a98c16b4e04409bcdeb
kushberrycream <tom@wilson-express.co.uk>
2021-05-25 11:41:55 +0000

    get static files working

--- wilsons_express/settings.py
+++ wilsons_express/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', '.gitpod.io',
-                 'wilsons-express-django.herokuapp.com',]
+                 'wilsons-express-django.herokuapp.com', ]
 
 # Application definition
 
@@ -211,6 +211,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
